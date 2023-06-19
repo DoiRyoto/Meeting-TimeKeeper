1de8f51c55d724d619c68a73b1336ef357c38588
DoiRyoto <102282406+DoiRyoto@users.noreply.github.com>
2023-06-19 18:23:18 +0900

    GONGさんのコードと統合

---
+++ back/end.php
@@ -0,0 +1,12 @@
+<?php
+define("ADDRESS", "192.168.32.32");
+define("USER", "pi");
+define("PASSWORD", "xxxxxxxxxxxxxxxx");
+
+If(!is_null($_GET["mode"])){
+$sconnection = ssh2_connect(ADDRESS, 22);
+ssh2_auth_password($sconnection, USER, PASSWORD);
+$command = "python"." "."end.py"." ".$_GET["mode"];
+$stdio_stream = ssh2_exec($sconnection, $command);
+}
+?>
